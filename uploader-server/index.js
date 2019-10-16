9e7854013b7393dffe44ab50afe16ee437224577
Simple <591831866@qq.com>
2019-10-16 09:38:54 +0800

    添加测试

--- uploader-server/index.js
+++ uploader-server/index.js
@@ -1,38 +1,42 @@
-var formidable = require('formidable');
-var uuid = require('node-uuid');
-var fs = require('fs');
-var express = require('express');
-var http = require('http');
+const formidable = require('formidable');
 
-var app = express();
-var allowCrossDomain = function(req, res, next) {
+const uuid = require('node-uuid');
+
+const fs = require('fs');
+
+const express = require('express');
+const http = require('http');
+
+const path = require('path');
+const pkg = require('../package.json');
+const webpack = require('webpack');
+
+const app = express();
+const allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-
     next();
 };
 app.use(allowCrossDomain);
 
 function node_upload(req, callback, next) {
-    var form = new formidable.IncomingForm();
+    const form = new formidable.IncomingForm();
     form.encoding = 'utf-8';
     form.uploadDir = __dirname + '/uploads/';
     form.maxFieldsSize = 10 * 1024 * 1024;
     //解析
     form.parse(req, function (err, fields, files) {
-        if(err) return callback(err);
+        if (err) return callback(err);
 
-        for (var file in files) {
+        for (const file in files) {
             //后缀名
-            var extName = /\.[^\.]+/.exec(files[file].name);
-            var ext = Array.isArray(extName)
-                ? extName[0]
-                : '';
+            const extName = /\.[^\.]+/.exec(files[file].name);
+            const ext = Array.isArray(extName) ? extName[0] : '';
             //重命名，以防文件重复
-            var avatarName = uuid() + ext;
+            const avatarName = uuid() + ext;
             //移动的文件目录
-            var newPath = form.uploadDir + avatarName;
+            const newPath = form.uploadDir + avatarName;
             fs.renameSync(files[file].path, newPath);
             fields[file] = {
                 size: files[file].size,
@@ -45,14 +49,18 @@ function node_upload(req, callback, next) {
         callback(null, fields);
     });
 }
-app.post('/upload', function(req, res, next) {
+
+app.post('/upload', function (req, res, next) {
     node_upload(req, function (err, fields) {
         // setTimeout(function(){
-            res.json(fields);
+        res.json(fields);
         // }, 1000);
     }, next);
 });
 
-http.createServer(app).listen(8002, function(){
+http.createServer(app).listen(8002, function () {
+    console.log(path.join('ggg', '22'));
+    console.log(__dirname);
+    console.log(path.resolve(__dirname, 'component'));
     console.log('Express server listening on port 8002');
 });
